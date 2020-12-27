d336800f352e8cf970fd804160bccc48512394c1
Aliraza786789 <lookatme786.ar@gmail.com>
2020-12-27 15:20:30 +0500

    INitializing the Scripts

---
+++ Assets/GooglePlayGamesSeriviesLoginScripts/GooglePlayGamesSeriviesLoginScripts.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+public class GooglePlayGamesSeriviesLoginScripts : MonoBehaviour
+{
+    public static PlayGamesPlatform GooglePlayGamesInstance;
+    static GooglePlayGamesSeriviesLoginScripts Instance;
+    public static GooglePlayGamesSeriviesLoginScripts _Instance
+    {
+        get
+        {
+            return Instance;
+        }
+        set
+        {
+            Instance = value;
+        }
+    }
+    
+    private void Awake()
+    {
+        DontDestroyOnLoad(this) ;
+        bool NullInstance = (Instance == null);
+        if (NullInstance)
+        {
+            _Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    void Start()
+    {
+        if (GooglePlayGamesInstance == null)
+        {
+            PlayGamesClientConfiguration configVariable = new PlayGamesClientConfiguration.Builder().Build();
+            PlayGamesPlatform.InitializeInstance(configVariable);
+            PlayGamesPlatform.DebugLogEnabled = true;
+        }
+        Social.localUser.Authenticate(sucess =>
+        {
+            if (sucess) { print("Loged in")}else { print("Failed to singin or signup")};
+        });
+
+    }
+    
+    void Update()
+    {
+        
+    }
+}
